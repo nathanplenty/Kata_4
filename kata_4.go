108ca41dc16fa1940781f5f4987826a3e97f8b45
nathanplenty <nathan.roskamp@plentysystems.com>
2023-08-23 14:22:43 +0200

    Update kata_4.go
    - better algorithm
    - better naming

--- kata_4.go
+++ kata_4.go
@@ -3,47 +3,28 @@ package main
 import "fmt"
 
 func longestCommonPrefix(str []string) string {
-	// 1. Get Slice of Strings (Array / Stack / like these "[]string" )
-	// 1.2. Check if enough Input is given
-	if len(str) < 2 {
-		return ""
-	}
-	c := ""
-	cn := ""
-	l := len(str)
-	// 2. Take Slice[0]
-	sl0 := str[0]
-	// 3. Take Slice[n] (n = +1)
-	n := 1
-	sln := str[n]
-	for {
-		cn = ""
-		sl := 0
-		sln = str[n]
-		// 3.1. Get shorter len of compared values
-		if len(str[0]) <= len(str[n]) {
-			sl = len(str[0])
-		} else {
-			sl = len(str[n])
+	// Initialize longest common prefix as string to LCP
+	LCP := ""
+	// Get shortest lenght of Input Words
+	short := len(str[0])
+	for i := 0; i < len(str); i++ {
+		if len(str[i]) < short {
+			short = len(str[i])
 		}
-		// 4. Check Slice[0] element[m] with Slice[n] element[m] (m = + 1)
-		for i := 0; i < sl; i++ {
-			// 4.1. If True -> append char
-			if sl0[i] == sln[i] {
-				c = c + string(sl0[i])
-				cn = cn + string(sl0[i])
+	}
+	// Get each character as i
+	for i := 0; i < short; i++ {
+		// Get each Word as j
+		for j := 0; j < len(str); j++ {
+			// Check if a collumn of characters is the same rune
+			if str[j][i] != str[0][i] {
+				return LCP
 			}
 		}
-		n = n + 1
-		if len(c) > len(cn) {
-			c = cn
-		}
-		// 5. Check if n >= n+1 ->
-		if n >= l {
-			break
-		}
+		// If collumn has same rune, add rune as character to LCP
+		LCP += string(str[0][i])
 	}
-	return c
+	return LCP
 }
 
 func main() {
